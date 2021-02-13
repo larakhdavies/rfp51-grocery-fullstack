160e09b29b2cabd29a31ea6dc166cc00e0708ae9
larakhdavies <lara.kh.davies@gmail.com>
2021-02-13 10:45:52 -1000

    ble to fetch data from DB in seperate file and connected react to server

---
+++ server/controllers.js
@@ -0,0 +1,18 @@
+const db = require("../db/index.js");
+
+module.exports = {
+  getAll: function (req, res) {
+    db.query('SELECT foodName from groceryList', (err, result) => {
+      if (err){
+        console.log("here err")
+        res.status(400);
+        res.send(err);
+      } else {
+        console.log(result)
+        res.status(200);
+        res.send(result);
+      }
+    })
+  }
+}
+
--- server/index.js
+++ server/index.js
@@ -5,8 +5,14 @@ const app = express();
 const port = 8080;
 const cors = require("cors");
 const db = require("../db/index.js"); //refers to dabase
+const controller = require("./controllers.js");
+
 
 app.use(cors());
+
+app.use(express.static('src'))
+
+app.get('/groceryList', controller.getAll)//takes in endpoitn and a handler
 
 app.get('/', (req, res) => {
   res.status(200);
