1df1881b46ba68103ff14ffe8bb2205a48f6bf8c
larakhdavies <lara.kh.davies@gmail.com>
2021-02-13 11:39:55 -1000

    dded groceryList comp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import GroceryList from "./GroceryList.jsx"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      list: "apple, banan"
+    };
   }
 
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <GroceryList list = {this.state.list} />
+        <div>
+          <h1>Hello, world!</h1>
+        </div>
       </div>
     );
   }
---
+++ client/src/GroceryList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GroceryList = (props) => {
+  return (
+    <div>
+      <h1>IM HEREEEE</h1>
+    </div>
+  )
+}
+
+export default GroceryList
